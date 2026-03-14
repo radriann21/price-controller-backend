658640f352fea97a7d1c61292042fb508a251a01
Adrian Rangel <adrianrangelargu@gmail.com>
2026-03-13 21:06:54 -0400

    fix: add category filter, cors method

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: [
       'Content-Type',
